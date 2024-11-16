ad976652fca2a0157845321c0a85f3bc85e7da95
Talha Jamal <chrislandson057@gmail.com>
2024-11-16 12:36:42 +0500

    Added

--- components/ColorPicker.tsx
+++ components/ColorPicker.tsx
@@ -20,7 +20,7 @@ function ColorPicker() {
 
   return (
     <div>
-      <div className="flex flex-col items-center justify-center h-screen bg-gray-100 dark:bg-gray-900 shadow-xl">
+      <div className="flex flex-col items-center justify-center h-screen bg-gray-300 dark:bg-gray-900 shadow-xl">
         <Card className="w-full max-w-md mx-auto p-8 grid gap-8">
           <div className="text-center space-y-2 text-lg">
             <CardTitle>Color Picker</CardTitle>
